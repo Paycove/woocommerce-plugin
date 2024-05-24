0c0ebfd5eef5cb76011f9601347994eefcc00a0f
Nate Finch <n8finch@gmail.com>
2024-05-24 17:09:58 -0500

    update readme

--- includes/class-wc-paycove-cart-to-checkout.php
+++ includes/class-wc-paycove-cart-to-checkout.php
@@ -7,7 +7,7 @@ class WC_Paycove_Cart_To_Checkout
      */
     public function __construct()
     {
-        remove_action('woocommerce_proceed_to_checkout', 'woocommerce_button_proceed_to_checkout', 20);
+        // remove_action('woocommerce_proceed_to_checkout', 'woocommerce_button_proceed_to_checkout', 20);
         add_action('woocommerce_proceed_to_checkout', array($this, 'custom_proceed_to_checkout_button'), 20);
     }
 
@@ -16,26 +16,26 @@ class WC_Paycove_Cart_To_Checkout
      */
     public function custom_proceed_to_checkout_button()
     {
-      // Get the checkout URL
-      $checkout_url = wc_get_checkout_url();
+        // Get the checkout URL
+        $checkout_url = wc_get_checkout_url();
 
-      // Get the cart total
-      $cart_total = WC()->cart->total;
+        // Get the cart total
+        $cart_total = WC()->cart->total;
 
-      // Get the account ID
-      $paycove_settings = get_option('woocommerce_paycove_settings', [] );
-      $account_id = $paycove_settings['paycove_account_id'] ?? 'no_key_found';
-      $invoice_template_id = $paycove_settings['paycove_invoice_template_id'] ?? 'no_key_found';
+        // Get the account ID
+        $paycove_settings = get_option('woocommerce_paycove_settings', []);
+        $account_id = $paycove_settings['paycove_account_id'] ?? 'no_key_found';
+        $invoice_template_id = $paycove_settings['paycove_invoice_template_id'] ?? 'no_key_found';
 
-      // Get current user name and email if logged in.
-      $current_user = wp_get_current_user();
-      $name = $current_user->display_name;
-      $email = $current_user->user_email;
+        // Get current user name and email if logged in.
+        $current_user = wp_get_current_user();
+        $name = $current_user->display_name;
+        $email = $current_user->user_email;
 
-      // Construct your custom URL with the total
-      $custom_url = 'https://staging.paycove.io/checkout-builder-form?type=invoice&invoice_template_id=' . $invoice_template_id . '&total=' . $cart_total . '&account_id=' . $account_id . '&adjustable_amount=true&full_name=' . $name . '&email=' . $email . '&line_items[0][name]=Payment for something';
-      
-      ?>
+        // Construct your custom URL with the total
+        $custom_url = 'https://staging.paycove.io/checkout-builder-form?type=invoice&invoice_template_id=' . $invoice_template_id . '&total=' . $cart_total . '&account_id=' . $account_id . '&adjustable_amount=true&full_name=' . $name . '&email=' . $email . '&line_items[0][name]=Payment for something';
+
+        ?>
       <a href="<?php echo esc_url($custom_url); ?>" class="checkout-button button alt wc-forward wp-element-button">
           Checkout on Paycove (Total: <?php echo wc_price($cart_total); ?>)
       </a>
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Paycove Payment Gateway
+
+WIP for Paycove.
+
+## Install
+
+You know the drill:
+
+```bash
+# Install
+npm install
+composer install
+
+# Build JS assets for plugin.
+npm start
+```
+
+## Formatting and linting
+
+See `composer.json` and `package.json` for available formatting and linting scripts.
+
+
+## Resources
+- 
+- https://rudrastyh.com/woocommerce/payment-gateway-plugin.html#gateway_processing
+- https://rudrastyh.com/woocommerce/checkout-block-payment-method-integration.html
