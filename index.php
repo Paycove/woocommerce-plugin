cdf180784b962aeec045a34bd7505e1ada2b2332
Nate Finch <n8finch@gmail.com>
2025-01-23 17:40:33 -0600

    update readme, version bump

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Description: Integrate your store with Paycove.
  * Author: Paycove
  * Author URI: https://paycove.io
- * Version: 0.1.6
+ * Version: 0.1.7
  * License: MIT
  * Text Domain: paycove
  */
